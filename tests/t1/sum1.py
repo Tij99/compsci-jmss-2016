743ba08e04d1f59c382cda727c29c436d689cade
Kshitij <kshitijsharma99@yahoo.in>
2016-02-29 15:15:26 +1100

    No space between words

--- tests/t1/sum1.py
+++ tests/t1/sum1.py
@@ -1,1 +1,13 @@
 # write a program that reads in 10 numbers, then prints the sum of those
+i = 1
+numbers = []  # List
+
+while i <= 10:
+    response = input("Enter number " and i)  # I am trying to print: Enter the (value of i), but and is not the correct code.
+    numbers.append(int(response))
+    i = i + 1
+
+while i > 10:
+    Sum = int(numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9])
+    print("The sum of the 10 numbers entered is " and Sum)
+    exit()
--- tests/t1/sum2.py
+++ tests/t1/sum2.py
@@ -1,2 +1,13 @@
 # copy the code from sum1.py into this file, THEN:
 # change your program so it keeps reading numbers until it gets a -1, then prints the sum of all numbers read
+
+i = 0
+Sum = 0
+
+
+while i == 0:
+    response = input("Enter number")
+    Sum += int(response)
+    if response == "-1":
+        print(Sum)
+        exit()
